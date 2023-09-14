c9aac5b245e2a2ccffa07c2a105d85d18e6b4dd8
Timur <timur.pnz@mail.ru>
2023-09-14 22:45:48 +0300

    Added a solution for 2 task

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,23 +1,51 @@
 ﻿// Задача 2: 
 // Напишите программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
+// *********************************************************************************
+// Console.WriteLine("Enter the first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter the second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+// {
+// Console.WriteLine($"Larger number: {a}");
+// Console.WriteLine($"Smaller numbers: {b}");
+// }
+// else if (b > a)
+// {
+// Console.WriteLine($"Larger number: {b}");
+// Console.WriteLine($"Smaller numbers: {a}");
+// }
+// else
+// {
+//     Console.WriteLine($"The numbers {a} and {b} are equal.");
+// }
+
+// Задача 4:
+// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
 Console.WriteLine("Enter the first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter the second number: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-if (a > b)
+Console.WriteLine("Enter the third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
 {
-Console.WriteLine($"Larger number: {a}");
-Console.WriteLine($"Smaller numbers: {b}");
+    max = b;
 }
-else if (b > a)
+
+if (c > max)
 {
-Console.WriteLine($"Larger number: {b}");
-Console.WriteLine($"Smaller numbers: {a}");
+    max = c;
 }
-else
-{
-    Console.WriteLine($"The numbers {a} and {b} are equal.");
-}
+
+Console.WriteLine($"Maximum number: {max}");
