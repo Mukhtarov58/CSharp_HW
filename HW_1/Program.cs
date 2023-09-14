b6a7d59ffdd95722eda68299fd7930b71783f640
Timur <timur.pnz@mail.ru>
2023-09-14 22:28:41 +0300

    Added a solution for 1 task

---
+++ HW_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: 
+// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+Console.WriteLine("Enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+Console.WriteLine($"Larger number: {a}");
+Console.WriteLine($"Smaller numbers: {b}");
+}
+else if (b > a)
+{
+Console.WriteLine($"Larger number: {b}");
+Console.WriteLine($"Smaller numbers: {a}");
+}
+else
+{
+    Console.WriteLine($"The numbers {a} and {b} are equal.");
+}
